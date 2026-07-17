2731a6f6abc318a908670d27e42f1fc8f882bd94
Utyf <utyff@mail.ru>
2026-07-17 19:58:22 +0300

    ll adc interleave

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,6 +58,7 @@ static void MX_TIM8_Init(void);
 static void MX_TIM4_Init(void);
 static void MX_DAC1_Init(void);
 static void MX_ADC2_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -114,6 +115,7 @@ int main(void)
   MX_TIM4_Init();
   MX_DAC1_Init();
   MX_ADC2_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -181,6 +183,111 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  LL_ADC_InitTypeDef ADC_InitStruct = {0};
+  LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
+  LL_ADC_CommonInitTypeDef ADC_CommonInitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  LL_RCC_SetADCClockSource(LL_RCC_ADC12_CLKSOURCE_PLL);
+
+  /* Peripheral clock enable */
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_ADC12);
+
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  /**ADC1 GPIO Configuration
+  PA0   ------> ADC1_IN1
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* ADC1 DMA Init */
+
+  /* ADC1 Init */
+  LL_DMA_SetPeriphRequest(DMA2, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_ADC1);
+
+  LL_DMA_SetDataTransferDirection(DMA2, LL_DMA_CHANNEL_1, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+
+  LL_DMA_SetChannelPriorityLevel(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PRIORITY_VERYHIGH);
+
+  LL_DMA_SetMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MODE_NORMAL);
+
+  LL_DMA_SetPeriphIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PERIPH_NOINCREMENT);
+
+  LL_DMA_SetMemoryIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MEMORY_INCREMENT);
+
+  LL_DMA_SetPeriphSize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PDATAALIGN_HALFWORD);
+
+  LL_DMA_SetMemorySize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MDATAALIGN_HALFWORD);
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  ADC_InitStruct.Resolution = LL_ADC_RESOLUTION_8B;
+  ADC_InitStruct.DataAlignment = LL_ADC_DATA_ALIGN_RIGHT;
+  ADC_InitStruct.LowPowerMode = LL_ADC_LP_MODE_NONE;
+  LL_ADC_Init(ADC1, &ADC_InitStruct);
+  ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
+  ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
+  ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
+  ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
+  ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_UNLIMITED;
+  ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
+  LL_ADC_REG_Init(ADC1, &ADC_REG_InitStruct);
+  LL_ADC_SetGainCompensation(ADC1, 0);
+  LL_ADC_SetOverSamplingScope(ADC1, LL_ADC_OVS_DISABLE);
+  ADC_CommonInitStruct.CommonClock = LL_ADC_CLOCK_SYNC_PCLK_DIV4;
+  ADC_CommonInitStruct.Multimode = LL_ADC_MULTI_DUAL_REG_INTERL;
+  ADC_CommonInitStruct.MultiDMATransfer = LL_ADC_MULTI_REG_DMA_UNLMT_RES8_6B;
+  ADC_CommonInitStruct.MultiTwoSamplingDelay = LL_ADC_MULTI_TWOSMP_DELAY_2CYCLES;
+  LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC1), &ADC_CommonInitStruct);
+
+  /* Disable ADC deep power down (enabled by default after reset state) */
+  LL_ADC_DisableDeepPowerDown(ADC1);
+  /* Enable ADC internal voltage regulator */
+  LL_ADC_EnableInternalRegulator(ADC1);
+  /* Delay for ADC internal voltage regulator stabilization. */
+  /* Compute number of CPU cycles to wait for, from delay in us. */
+  /* Note: Variable divided by 2 to compensate partially */
+  /* CPU processing cycles (depends on compilation optimization). */
+  /* Note: If system core clock frequency is below 200kHz, wait time */
+  /* is only a few CPU processing cycles. */
+  uint32_t wait_loop_index;
+  wait_loop_index = ((LL_ADC_DELAY_INTERNAL_REGUL_STAB_US * (SystemCoreClock / (100000 * 2))) / 10);
+  while(wait_loop_index != 0)
+  {
+    wait_loop_index--;
+  }
+
+  /** Configure Regular Channel
+  */
+  LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_1);
+  LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_1, LL_ADC_SAMPLINGTIME_2CYCLES_5);
+  LL_ADC_SetChannelSingleDiff(ADC1, LL_ADC_CHANNEL_1, LL_ADC_SINGLE_ENDED);
+  /* USER CODE BEGIN ADC1_Init 2 */
+  LL_ADC_StartCalibration(ADC1, LL_ADC_SINGLE_ENDED);
+  while (LL_ADC_IsCalibrationOnGoing(ADC1) != 0) {}
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
   * @brief ADC2 Initialization Function
   * @param None
   * @retval None
@@ -194,7 +301,6 @@ static void MX_ADC2_Init(void)
 
   LL_ADC_InitTypeDef ADC_InitStruct = {0};
   LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
-  LL_ADC_CommonInitTypeDef ADC_CommonInitStruct = {0};
 
   LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
@@ -212,25 +318,6 @@ static void MX_ADC2_Init(void)
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /* ADC2 DMA Init */
-
-  /* ADC2 Init */
-  LL_DMA_SetPeriphRequest(DMA2, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_ADC2);
-
-  LL_DMA_SetDataTransferDirection(DMA2, LL_DMA_CHANNEL_1, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
-
-  LL_DMA_SetChannelPriorityLevel(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PRIORITY_VERYHIGH);
-
-  LL_DMA_SetMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MODE_NORMAL);
-
-  LL_DMA_SetPeriphIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PERIPH_NOINCREMENT);
-
-  LL_DMA_SetMemoryIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MEMORY_INCREMENT);
-
-  LL_DMA_SetPeriphSize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PDATAALIGN_BYTE);
-
-  LL_DMA_SetMemorySize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MDATAALIGN_BYTE);
-
   /* USER CODE BEGIN ADC2_Init 1 */
 
   /* USER CODE END ADC2_Init 1 */
@@ -241,17 +328,14 @@ static void MX_ADC2_Init(void)
   ADC_InitStruct.DataAlignment = LL_ADC_DATA_ALIGN_RIGHT;
   ADC_InitStruct.LowPowerMode = LL_ADC_LP_MODE_NONE;
   LL_ADC_Init(ADC2, &ADC_InitStruct);
-  ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
   ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
   ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
   ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
-  ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_LIMITED; // cube bug
+  ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_NONE;
   ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
   LL_ADC_REG_Init(ADC2, &ADC_REG_InitStruct);
   LL_ADC_SetGainCompensation(ADC2, 0);
   LL_ADC_SetOverSamplingScope(ADC2, LL_ADC_OVS_DISABLE);
-  ADC_CommonInitStruct.CommonClock = LL_ADC_CLOCK_ASYNC_DIV2;
-  LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC2), &ADC_CommonInitStruct);
 
   /* Disable ADC deep power down (enabled by default after reset state) */
   LL_ADC_DisableDeepPowerDown(ADC2);
--- usr/src/adc.c
+++ usr/src/adc.c
@@ -13,124 +13,164 @@
 typedef struct ADC_param {
     uint32_t ADC_Prescaler;
     uint32_t ADC_SampleCycles;
-    float SampleTime;    // microseconds
+    float SampleTime; // microseconds
 } ADC_PARAM;
 
 #define ADC_Parameters_Size 6
 static const ADC_PARAM ADC_Parameters[ADC_Parameters_Size] = {
-        {LL_ADC_CLOCK_ASYNC_DIV1,  LL_ADC_SAMPLINGTIME_2CYCLES_5,  0.f},
-        {LL_ADC_CLOCK_ASYNC_DIV1,  LL_ADC_SAMPLINGTIME_6CYCLES_5,  0.f},
-        {LL_ADC_CLOCK_ASYNC_DIV2,  LL_ADC_SAMPLINGTIME_2CYCLES_5,  0.f},
-        {LL_ADC_CLOCK_ASYNC_DIV1,  LL_ADC_SAMPLINGTIME_12CYCLES_5, 0.f},
-        {LL_ADC_CLOCK_ASYNC_DIV2,  LL_ADC_SAMPLINGTIME_6CYCLES_5,  0.f},
-        {LL_ADC_CLOCK_ASYNC_DIV4,  LL_ADC_SAMPLINGTIME_2CYCLES_5,  0.f}
+    {LL_ADC_CLOCK_ASYNC_DIV1, LL_ADC_SAMPLINGTIME_2CYCLES_5, 0.f},
+    {LL_ADC_CLOCK_ASYNC_DIV1, LL_ADC_SAMPLINGTIME_6CYCLES_5, 0.f},
+    {LL_ADC_CLOCK_ASYNC_DIV2, LL_ADC_SAMPLINGTIME_2CYCLES_5, 0.f},
+    {LL_ADC_CLOCK_ASYNC_DIV1, LL_ADC_SAMPLINGTIME_12CYCLES_5, 0.f},
+    {LL_ADC_CLOCK_ASYNC_DIV2, LL_ADC_SAMPLINGTIME_6CYCLES_5, 0.f},
+    {LL_ADC_CLOCK_ASYNC_DIV4, LL_ADC_SAMPLINGTIME_2CYCLES_5, 0.f}
 };
 
 uint32_t ADC_Prescaler = LL_ADC_CLOCK_ASYNC_DIV2;
 uint32_t ADC_SampleTime = LL_ADC_SAMPLINGTIME_2CYCLES_5;
-float    ADC_MeasureTime = 0;
-uint8_t  ADC_param = 3;
+float ADC_MeasureTime = 0;
+uint8_t ADC_param = 2;
 
-uint16_t ScreenTime = 0;      // index in ScreenTimes
-uint16_t ScreenTime_adj = 0;  // 0-9 shift in ScreenTime
-const float ScreenTimes[] = {100, 200, 500, 1000, 2000, 5000, 10000, 20000};  // sweep screen, microseconds
+uint16_t ScreenTime = 0; // index in ScreenTimes
+uint16_t ScreenTime_adj = 0; // 0-9 shift in ScreenTime
+const float ScreenTimes[] = {100, 200, 500, 1000, 2000, 5000, 10000, 20000}; // sweep screen, microseconds
 
-uint8_t  ADCworks;
-uint32_t ADCStartTick;         // time when start ADC buffer fill
-uint32_t ADCHalfElapsedTick;   // the last time half buffer fill
-uint32_t ADCElapsedTick;       // the last time buffer fill
+uint8_t ADCworks;
+uint32_t ADCStartTick; // time when start ADC buffer fill
+uint32_t ADCHalfElapsedTick; // the last time half buffer fill
+uint32_t ADCElapsedTick; // the last time buffer fill
 
+static void ADC_stop(void);
 static void ADC2_Init(void);
+static void ADC1_Init(void);
 static float ADC_calcSampleTime();
 
 
 void ADC_start() {
-
     if (ADCworks != 0) {
         return;
     }
     ADCworks = 1;
 
+    ADC_stop();
+    LL_DMA_DisableChannel(DMA2, LL_DMA_CHANNEL_1);
+    LL_mDelay(1);
+
+    ADC1_Init();
     ADC2_Init();
 
     LL_ADC_ClearFlag_ADRDY(ADC2);
     LL_ADC_Enable(ADC2);
+    LL_ADC_ClearFlag_ADRDY(ADC1);
+    LL_ADC_Enable(ADC1);
     while (!LL_ADC_IsActiveFlag_ADRDY(ADC2)) {}
     LL_ADC_ClearFlag_ADRDY(ADC2);
+    while (!LL_ADC_IsActiveFlag_ADRDY(ADC1)) {}
+    LL_ADC_ClearFlag_ADRDY(ADC1);
 
     // Set DMA transfer addresses of source and destination
     LL_DMA_ConfigAddresses(DMA2, LL_DMA_CHANNEL_1,
-                           (uint32_t) &(ADC2->DR),
-                           (uint32_t)&samplesBuffer,
+                           (uint32_t) &(ADC12_COMMON->CDR),
+                           (uint32_t) &samplesBuffer,
                            LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
     // Set DMA transfer size
-    LL_DMA_SetDataLength(DMA2, LL_DMA_CHANNEL_1, BUF_SIZE);
-    // Enable DMA transfer interruption: transfer error
+    LL_DMA_SetDataLength(DMA2, LL_DMA_CHANNEL_1, BUF_SIZE / 2);
+    // Enable DMA transfer interruption: transfer complete & error
     LL_DMA_EnableIT_TC(DMA2, LL_DMA_CHANNEL_1);
     LL_DMA_EnableIT_TE(DMA2, LL_DMA_CHANNEL_1);
     LL_DMA_EnableChannel(DMA2, LL_DMA_CHANNEL_1);
 
-    LL_ADC_REG_StartConversion(ADC2);
+    LL_ADC_REG_StartConversion(ADC1);
 
     ADCStartTick = DWT_Get_Current_Tick();
 }
 
 static void ADC_stop() {
-    LL_ADC_REG_StopConversion(ADC2);
-    while (LL_ADC_REG_IsConversionOngoing(ADC2)) {}
+    LL_ADC_REG_StopConversion(ADC1);
+    while (LL_ADC_REG_IsConversionOngoing(ADC1)) {}
 
-    LL_ADC_Disable(ADC2);
-    while (LL_ADC_IsDisableOngoing(ADC2)) {}
-    while (LL_ADC_IsEnabled(ADC2)) {}
+    if (LL_ADC_IsEnabled(ADC1)) {
+        LL_ADC_Disable(ADC1);
+        while (LL_ADC_IsDisableOngoing(ADC1)) {}
+        while (LL_ADC_IsEnabled(ADC1)) {}
+    }
 
-    LL_DMA_DisableChannel(DMA2, LL_DMA_CHANNEL_1);
-    LL_mDelay(1);
+    if (LL_ADC_IsEnabled(ADC2)) {
+        LL_ADC_Disable(ADC2);
+        while (LL_ADC_IsDisableOngoing(ADC2)) {}
+        while (LL_ADC_IsEnabled(ADC2)) {}
+    }
+}
+
+static void ADC1_Init(void) {
+    LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
+    LL_ADC_CommonInitTypeDef ADC_CommonInitStruct = {0};
+
+    // DMA Init
+    LL_DMA_SetPeriphRequest(DMA2, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_ADC1);
+    LL_DMA_SetDataTransferDirection(DMA2, LL_DMA_CHANNEL_1, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+    LL_DMA_SetChannelPriorityLevel(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PRIORITY_VERYHIGH);
+    LL_DMA_SetMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MODE_NORMAL);
+    LL_DMA_SetPeriphIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PERIPH_NOINCREMENT);
+    LL_DMA_SetMemoryIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MEMORY_INCREMENT);
+    LL_DMA_SetPeriphSize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PDATAALIGN_HALFWORD);
+    LL_DMA_SetMemorySize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MDATAALIGN_HALFWORD);
+
+    // ADC1 init
+    MODIFY_REG(ADC1->CFGR, ADC_CFGR_RES | ADC_CFGR_ALIGN | ADC_CFGR_AUTDLY,
+               LL_ADC_RESOLUTION_8B | LL_ADC_DATA_ALIGN_RIGHT | LL_ADC_LP_MODE_NONE);
+
+    ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
+    ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
+    ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
+    ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
+    ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_LIMITED;
+    ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
+    LL_ADC_REG_Init(ADC1, &ADC_REG_InitStruct);
+    LL_ADC_SetGainCompensation(ADC1, 0);
+    LL_ADC_SetOverSamplingScope(ADC1, LL_ADC_OVS_DISABLE);
+
+    // Common config
+    ADC_CommonInitStruct.CommonClock = ADC_Prescaler;
+    ADC_CommonInitStruct.Multimode = LL_ADC_MULTI_DUAL_REG_INTERL;
+    ADC_CommonInitStruct.MultiDMATransfer = LL_ADC_MULTI_REG_DMA_UNLMT_RES8_6B;
+    ADC_CommonInitStruct.MultiTwoSamplingDelay = LL_ADC_MULTI_TWOSMP_DELAY_2CYCLES; // TODO
+    LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC1), &ADC_CommonInitStruct);
+    CLEAR_BIT(ADC12_COMMON->CCR, ADC_CCR_DMACFG); // cube ll bug
+
+    // Configure Regular Channel
+    LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_1);
+    LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_1, ADC_SampleTime);
+    LL_ADC_SetChannelSingleDiff(ADC1, LL_ADC_CHANNEL_1, LL_ADC_SINGLE_ENDED);
 }
 
 static void ADC2_Init(void) {
+    LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
 
-  if (LL_ADC_IsEnabled(ADC2)) {
-      ADC_stop();
-  }
+    MODIFY_REG(ADC2->CFGR, ADC_CFGR_RES | ADC_CFGR_ALIGN | ADC_CFGR_AUTDLY,
+               LL_ADC_RESOLUTION_8B | LL_ADC_DATA_ALIGN_RIGHT | LL_ADC_LP_MODE_NONE);
 
-  // ADC2 DMA Init
-  LL_DMA_SetPeriphRequest(DMA2, LL_DMA_CHANNEL_1, LL_DMAMUX_REQ_ADC2);
-  LL_DMA_SetDataTransferDirection(DMA2, LL_DMA_CHANNEL_1, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
-  LL_DMA_SetChannelPriorityLevel(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PRIORITY_VERYHIGH);
-  LL_DMA_SetMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MODE_NORMAL);
-  LL_DMA_SetPeriphIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PERIPH_NOINCREMENT);
-  LL_DMA_SetMemoryIncMode(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MEMORY_INCREMENT);
-  LL_DMA_SetPeriphSize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_PDATAALIGN_BYTE);
-  LL_DMA_SetMemorySize(DMA2, LL_DMA_CHANNEL_1, LL_DMA_MDATAALIGN_BYTE);
+    ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
+    ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
+    ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
+    ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
+    ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_NONE;
+    ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
+    LL_ADC_REG_Init(ADC2, &ADC_REG_InitStruct);
+    LL_ADC_SetGainCompensation(ADC2, 0);
+    LL_ADC_SetOverSamplingScope(ADC2, LL_ADC_OVS_DISABLE);
 
-  MODIFY_REG(ADC2->CFGR, ADC_CFGR_RES | ADC_CFGR_ALIGN | ADC_CFGR_AUTDLY,
-             LL_ADC_RESOLUTION_8B | LL_ADC_DATA_ALIGN_RIGHT | LL_ADC_LP_MODE_NONE);
-
-  LL_ADC_REG_InitTypeDef ADC_REG_InitStruct = {0};
-  LL_ADC_CommonInitTypeDef ADC_CommonInitStruct = {0};
-  ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
-  ADC_REG_InitStruct.SequencerLength = LL_ADC_REG_SEQ_SCAN_DISABLE;
-  ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
-  ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
-  ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_LIMITED; // cube bug
-  ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
-  LL_ADC_REG_Init(ADC2, &ADC_REG_InitStruct);
-  LL_ADC_SetGainCompensation(ADC2, 0);
-  LL_ADC_SetOverSamplingScope(ADC2, LL_ADC_OVS_DISABLE);
-  ADC_CommonInitStruct.CommonClock = ADC_Prescaler;
-  LL_ADC_CommonInit(__LL_ADC_COMMON_INSTANCE(ADC2), &ADC_CommonInitStruct);
-
-  // Configure Regular Channel
-  LL_ADC_REG_SetSequencerRanks(ADC2, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_3);
-  LL_ADC_SetChannelSamplingTime(ADC2, LL_ADC_CHANNEL_3, ADC_SampleTime);
-  LL_ADC_SetChannelSingleDiff(ADC2, LL_ADC_CHANNEL_3, LL_ADC_SINGLE_ENDED);
+    // Configure Regular Channel
+    LL_ADC_REG_SetSequencerRanks(ADC2, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_3);
+    LL_ADC_SetChannelSamplingTime(ADC2, LL_ADC_CHANNEL_3, ADC_SampleTime);
+    LL_ADC_SetChannelSingleDiff(ADC2, LL_ADC_CHANNEL_3, LL_ADC_SINGLE_ENDED);
 }
 
 
 void ADC_step(int16_t step) {
     if (step == 0) return;
     if (step > 0) {
-        if (ADC_param < ADC_Parameters_Size-1) ADC_param++;
+        if (ADC_param < ADC_Parameters_Size - 1) ADC_param++;
     } else {
         if (ADC_param > 0) ADC_param--;
     }
@@ -214,5 +254,5 @@ float ADC_calcSampleTime() {
             Error_Handler();
     }
 
-    return (CONV_TICS + sampling) * 1/(ADC_CLOCK/presc /1000000.f) * 1000.f /2.f ;
+    return (CONV_TICS + sampling) * 1 / (ADC_CLOCK / presc / 1000000.f) * 1000.f / 2.f;
 }
