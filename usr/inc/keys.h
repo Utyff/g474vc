b02e324ddd7b0f2ab178353671b7657693cfc45a
Utyf <utyff@mail.ru>
2026-09-07 19:29:38 +0300

    new pcb & functionality

--- usr/inc/keys.h
+++ usr/inc/keys.h
@@ -8,6 +8,8 @@
 #define BUTTON2 0x02
 #define BUTTON3 0x04
 #define BUTTON4 0x08
+#define BUTTON5 0x10
+#define BUTTON6 0x20
 
 extern uint8_t button1Count;
 extern uint8_t button2Count;
@@ -16,6 +18,7 @@ extern uint16_t btns_state;
 
 void KEYS_init();
 void KEYS_scan();
-int16_t ENC_Get();
+int16_t ENC1_Get();
+int16_t ENC2_Get();
 
 #endif //KEYS_H
